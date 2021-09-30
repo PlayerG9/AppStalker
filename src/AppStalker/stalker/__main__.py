9d8ba2c6fa183e501183be44cdedce4494e3da34
PlayerG9 <g9yout@gmail.com>
2021-09-30 17:34:31 +0200

    setup processview.py, moved example code to processview.py

--- src/AppStalker/stalker/__main__.py
+++ src/AppStalker/stalker/__main__.py
@@ -1,35 +1,6 @@
 # -*- coding=utf-8 -*-
 r"""
-while True:
-    user32 = ctypes.windll.user32
-    h_wnd = user32.GetForegroundWindow()
-    pid = wintypes.DWORD()
-    user32.GetWindowThreadProcessId(h_wnd, ctypes.byref(pid))
 
-    process_id = pid.value
-    process = psutil.Process(pid.value)
-    process_name = process.name()
-    process_start = str(process)[-10:-2]
-    process_file = process.cmdline()
-
-    if process_name not in process_list.keys():
-        process_list[process_name] = [process_id, [process_start], process_file]
-
-    if process_start not in process_list[process_name][1]:
-        process_list[process_name][1].append(process_start)
-
-    if lastprocess != process_name and process_name not in ["System Idle Process"]:
-        cur_time = time.asctime(time.localtime(time.time())).split(" ")[3]
-        time_list[-1] = [cur_time, process_name]
-        time_list.append([])
-
-    lastprocess = process_name
-    time.sleep(1)
-    counter += 1
-    if counter >= countermax:
-        counter = 0
-        time_list[-1] = [time.asctime(time.localtime(time.time())).split(" ")[3], process_name]
-        save()
 """
 from tendo.singleton import SingleInstance
 me = SingleInstance()
@@ -43,8 +14,10 @@ import threading
 import os
 import time
 import logging
+import sqlite3 as sql
 
 import scripts
+import processview
 
 
 class Application:
@@ -87,7 +60,7 @@ class Application:
         self.icon.stop()
 
     def task(self):
-        self.icon.notify(title="Measuring", message=__file__)
+        pass
 
     ####################################################################################################################
 
---
+++ src/AppStalker/stalker/processview.py
@@ -0,0 +1,33 @@
+# -*- coding=utf-8 -*-
+r"""
+while True:
+    user32 = ctypes.windll.user32
+    h_wnd = user32.GetForegroundWindow()
+    pid = wintypes.DWORD()
+    user32.GetWindowThreadProcessId(h_wnd, ctypes.byref(pid))
+
+    process_id = pid.value
+    process = psutil.Process(pid.value)
+    process_name = process.name()
+    process_start = str(process)[-10:-2]
+    process_file = process.cmdline()
+
+    if process_name not in process_list.keys():
+        process_list[process_name] = [process_id, [process_start], process_file]
+
+    if process_start not in process_list[process_name][1]:
+        process_list[process_name][1].append(process_start)
+
+    if lastprocess != process_name and process_name not in ["System Idle Process"]:
+        cur_time = time.asctime(time.localtime(time.time())).split(" ")[3]
+        time_list[-1] = [cur_time, process_name]
+        time_list.append([])
+
+    lastprocess = process_name
+    time.sleep(1)
+    counter += 1
+    if counter >= countermax:
+        counter = 0
+        time_list[-1] = [time.asctime(time.localtime(time.time())).split(" ")[3], process_name]
+        save()
+"""
