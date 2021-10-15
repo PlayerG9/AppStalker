8f0b373b13640257b23326c31663cba6154fbbbb
PlayerG9 <g9yout@gmail.com>
2021-10-15 15:27:06 +0200

    improved runtime by caching resolved path

--- src/AppStalker/scripts.py
+++ src/AppStalker/scripts.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import functools
 
 
+@functools.cache
 def get_appdir() -> str:
     if getattr(sys, 'frozen', False):  # running as .exe
         return os.path.dirname(__file__)
@@ -9,5 +11,11 @@ def get_appdir() -> str:
         return os.path.dirname(__file__)
 
 
+@functools.cache
 def get_memdir() -> str:
     return os.path.join(get_appdir(), 'memory')
+
+
+@functools.cache
+def get_dbfile() -> str:
+    return os.path.join(get_appdir(), 'memory', 'data.sl3')
